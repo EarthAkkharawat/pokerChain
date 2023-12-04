7bf005741da1fa6fffb2c9380e2541e3b37d4faf
Akkharawat <akkharawatbct@gmail.com>
2023-12-05 00:33:17 +0700

    minor fixed

--- frontend/src/utils/contracts.tsx
+++ frontend/src/utils/contracts.tsx
@@ -3,13 +3,13 @@ import { ethers } from 'ethers';
 import pokerGameABI from './pokerContractABI.json';
 import nftABI from './nftContractABI.json';
 
-const contractAddress = '0x28c05D4A6163BaFa0B7D6825fAD8a304B95A8A4B';
+const pokerContractAddress = '0x28c05D4A6163BaFa0B7D6825fAD8a304B95A8A4B';
 const nftContractAddress = '0x4472d0910B2cA1dD0119DBffC80f5A286b2F0d00';
 
 export const getEthereumContract = async () => {
   const provider = new ethers.BrowserProvider(window.ethereum);
   const signer = await provider.getSigner();
-  const contract = new ethers.Contract(contractAddress, pokerGameABI, signer);
+  const contract = new ethers.Contract(pokerContractAddress, pokerGameABI, signer);
   return contract;
 };
 
