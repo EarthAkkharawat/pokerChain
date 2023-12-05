4f6193619e2c26305fdf012d6e64332eff6bba3f
Akira Sitdhikariyawat <akira@192.168.68.104>
2023-12-05 17:48:14 +0700

    ??

--- frontend/src/components/table/GameTable.tsx
+++ frontend/src/components/table/GameTable.tsx
@@ -23,22 +23,34 @@ const Table: React.FC = () => {
   // const navigate = useNavigate();
   const [contract, setContract] = useState<any>(null);
   const [gameId, setGameId] = useState<number>(-1);
+  const [gameStatus, setGameStatus] = useState<number>(1);
   const [tableCards, setTableCards] = useState<number[]>([255,255,255,255,255]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchContract = async () => {
       const fetchedContract = await getPokerGameContract();
       setContract(fetchedContract);
       setGameId(Number(window.location.pathname.split("/")[2]));
     };
-    fetchData();
+    fetchContract();
     // console.log("contract:", contract);
   }, []); 
 
   useEffect(() => {
+    const fetchGameDetails = async () => {
+        if (contract) {
+            const gameDetails = await contract.getGameBasicDetails(gameId);
+            setGameStatus(gameDetails.status);
+        }
+    };
+    fetchGameDetails();
+  }, [contract, gameId]);
+
+  useEffect(() => {
     console.log("contract:", contract);
     console.log("gameId:", gameId);
-  }, [contract, gameId]); 
+    console.log("gameStatus:", gameStatus);
+  }, [contract, gameId, gameStatus]); 
 
   // useState(() => {
   //   const fetchData = async() => {
@@ -54,6 +66,7 @@ const Table: React.FC = () => {
           seed
         );
         await tx.wait();
+        console.log("Game started");
       } catch (error) {
         alert(error);
       }
@@ -65,21 +78,28 @@ const Table: React.FC = () => {
       className="background-container justify-content-between text-center"
       style={{ backgroundImage: `url(${pokerBG})` }}
     >
-      <Button onClick={() => startGame(1)}>STARTTTTTTTTT</Button>
-      <Row className="mt-5">
-        <Col>
-          <OtherPlayer />
-        </Col>
-        <Col>
-          <OtherPlayer playerLeft={false} />
-        </Col>
-      </Row>
-      <Row className="my-5">
-        <CardRow cards={tableCards} />
-      </Row>
-      <Row className="mb-5">
-        <SelfPlayer playerCards={MOCK_PLAYER_CARDS} />
-      </Row>
+      {false && (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}><Button variant="light" style = {{}} onClick={() => startGame(1)}>START GAME</Button></div>
+      )}
+
+      {(gameStatus > 2 || true) && (
+        <>
+          <Row className="mt-5">
+            <Col>
+              <OtherPlayer />
+            </Col>
+            <Col>
+              <OtherPlayer playerLeft={false} />
+            </Col>
+          </Row>
+          <Row className="my-5">
+            <CardRow cards={tableCards} />
+          </Row>
+          <Row className="mb-5">
+            <SelfPlayer playerCards={MOCK_PLAYER_CARDS} />
+          </Row>
+        </>
+      )}
     </Container>
   );
 };
